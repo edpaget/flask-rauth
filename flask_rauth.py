2951db33dc2b63c85146ff5159d54d90dda6cb6a
Edward Paget <edward@cassetteta.pe>
2015-10-20 12:55:52 -0500

    Update for Python3

--- flask_rauth.py
+++ flask_rauth.py
@@ -11,7 +11,7 @@
     :license: BSD, see LICENSE for more details.
 '''
 from functools import wraps
-from urlparse import urljoin
+from urllib.parse import urljoin
 from flask import request, session, redirect, current_app
 from werkzeug import parse_options_header
 from rauth.service import OAuth2Service, OAuth1Service, OflyService, Response, parse_utf8_qsl
